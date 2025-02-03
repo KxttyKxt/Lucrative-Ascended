37522b4784933041c6f20154ff94d4db60df9cda
Kat R. <56617952+KxttyKxt@users.noreply.github.com>
2025-02-03 10:12:43 -0500

    Make maps support more materials
    A compass for every map I have to scribble on? With such emphasis on cartography and map-making, the barrier to entry should be quite thin.

---
+++ kubejs/server_scripts/map_materials.js
@@ -0,0 +1,10 @@
+ServerEvents.tags('item', event => {
+    // minecraft:map
+
+    event.add('lucrative:map_writing_materials', '#minecraft:compasses')
+
+    event.add('lucrative:map_writing_materials', '#c:coal')
+    event.add('lucrative:map_writing_materials', 'minecraft:ink_sac')
+    event.add('lucrative:map_writing_materials', '#c:chalks')
+    event.add('lucrative:map_writing_materials', 'amendments:dye_bottle')
+})
